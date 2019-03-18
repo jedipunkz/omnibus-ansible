0f862aa35bc7df22c44fb82d6ac9e20964e624a7
Tomokazu HIRAI <t.hirai@kidsdiary.jp>
2019-03-18 16:55:52 +0900

    less も追加した

--- ansible_install_amazonlinux.sh
+++ ansible_install_amazonlinux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# for amazon linux 2
 
 # yum install -y python3-pip
 # pip3 install ansible
 sudo amazon-linux-extras install -y epel
 sudo yum install -y ansible
-sudo yum install -y procps
+sudo yum install -y procps less
